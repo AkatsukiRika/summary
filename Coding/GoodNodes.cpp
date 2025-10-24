027ea6f4c5c8976752d7ba1d394ae08d7af4330b
AkatsukiRika <m1345860061@gmail.com>
2025-10-24 11:13:22 +0800

    feat: 1448.统计二叉树中好节点的数目

--- Coding/GoodNodes.cpp
+++ Coding/GoodNodes.cpp
@@ -13,45 +13,69 @@ struct TreeNode {
 
 class GoodNodes {
 public:
-    void dfs(TreeNode* node, int maxValue = 0) {
-        printf("dfs current=%d, maxValue=%d\n", node->val, maxValue);
+    int goodNodesCount = 0;
+
+    void dfs(TreeNode* node, int maxValue) {
+        // printf("dfs current=%d, maxValue=%d\n", node->val, maxValue);
+        if (node->val >= maxValue) {
+            goodNodesCount++;
+            // printf("goodNodesCount=%d\n", goodNodesCount);
+        }
         if (node == nullptr) {
             return;
         }
         if (node->left != nullptr) {
-            printf("dfs (%d, %d) go left\n", node->val, maxValue);
-            dfs(node->left);
-            printf("dfs (%d, %d) recovered from left\n", node->val, maxValue);
+            // printf("dfs (%d, %d) go left\n", node->val, maxValue);
+            if (node->val >= maxValue) {
+                dfs(node->left, node->val);
+            } else {
+                dfs(node->left, maxValue);
+            }
+            // printf("dfs (%d, %d) recovered from left\n", node->val, maxValue);
         }
         if (node->right != nullptr) {
-            printf("dfs (%d, %d) go right\n", node->val, maxValue);
-            dfs(node->right);
-            printf("dfs (%d, %d) recovered from right\n", node->val, maxValue);
+            // printf("dfs (%d, %d) go right\n", node->val, maxValue);
+            if (node->val >= maxValue) {
+                dfs(node->right, node->val);
+            } else {
+                dfs(node->right, maxValue);
+            }
+            // printf("dfs (%d, %d) recovered from right\n", node->val, maxValue);
         }
     }
 
     int goodNodes(TreeNode* root) {
         dfs(root, root->val);
-        return 0;
+        return goodNodesCount;
     }
 };
 
 int main() {
+    // std::vector<TreeNode*> nodes = {
+    //     new TreeNode(3),
+    //     new TreeNode(1),
+    //     new TreeNode(4),
+    //     new TreeNode(3),
+    //     new TreeNode(1),
+    //     new TreeNode(5)
+    // };
+    // nodes[0]->left = nodes[1];
+    // nodes[0]->right = nodes[2];
+    // nodes[1]->left = nodes[3];
+    // nodes[2]->left = nodes[4];
+    // nodes[2]->right = nodes[5];
+
     std::vector<TreeNode*> nodes = {
         new TreeNode(3),
-        new TreeNode(1),
-        new TreeNode(4),
         new TreeNode(3),
-        new TreeNode(1),
-        new TreeNode(5)
+        new TreeNode(4),
+        new TreeNode(2)
     };
     nodes[0]->left = nodes[1];
-    nodes[0]->right = nodes[2];
-    nodes[1]->left = nodes[3];
-    nodes[2]->left = nodes[4];
-    nodes[2]->right = nodes[5];
+    nodes[1]->left = nodes[2];
+    nodes[1]->right = nodes[3];
 
-    auto solution = std::unique_ptr<GoodNodes>();
+    auto solution = std::make_unique<GoodNodes>();
     int answer = solution->goodNodes(nodes[0]);
     printf("answer=%d\n", answer);
 
